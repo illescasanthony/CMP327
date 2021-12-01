744dfa880ac92fa7c20aa6cd0684786261446343
illescasanthony <51797023+illescasanthony@users.noreply.github.com>
2021-12-01 11:14:19 -0800

    Delete IMDB_Rating-Histogram.R

--- IMDB_Rating-Histogram.R
+++
@@ -1,7 +0,0 @@
-oscars_df <- read.csv("~/Downloads/oscars_df.csv")
-#plot(oscars_df[,'IMDB.Rating'])
-hist(oscars_df$IMDB.Rating, main = "IMDB Distribution"
-     , xlab = "Rating"
-     , ylab = "Frequency"
-     , xlim=c(5.6,9.5)
-     , ylim=c(0,250))
